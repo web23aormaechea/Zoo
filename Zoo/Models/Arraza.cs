bb1187dfcabff08826309a714613020b0d7e1b3b
web23aormaechea <148438404+web23aormaechea@users.noreply.github.com>
2024-03-13 09:46:08 +0100

    arraza ikutuak

--- Zoo/Models/Arraza.cs
+++ Zoo/Models/Arraza.cs
@@ -7,7 +7,7 @@ namespace Zoo.Models
         [Key]
         public int ID { get; set; }
         public string Izena { get; set; }
-        public string? Mota { get; set;}
+        public string? Deskribapena { get; set;}
         public string? ImageUrl { get; set; }
         public int? ID_lekua { get; set; }
     }
